f2835a52e135d4d7a89a329a3976f891dc000fa2
ElijahButers <Elijahbuters@gmail.com>
2016-06-16 12:34:14 +0300

    Added a new Cocoa Class named ChordController that subclasses WKInterfaceController.

---
+++ ChordHelper WatchKit App/ChordController.swift
@@ -0,0 +1,31 @@
+//
+//  ChordController.swift
+//  ChordHelper
+//
+//  Created by User on 6/16/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ChordController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
